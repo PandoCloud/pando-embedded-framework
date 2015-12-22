e111149c0a55353db2da1fa0f9d4f9981910a402
leeshine <lichgu@aliyun.com>
2015-12-22 17:34:08 +0800

    modify pando timer interface.

--- framework/platform/include/pando_timer.h
+++ framework/platform/include/pando_timer.h
@@ -14,6 +14,8 @@
 
 #include "pando_types.h"
 
+typedef void (*expiry_cb)(void* arg);
+
 //define the timer structure
 struct pd_timer
 {
@@ -21,32 +23,18 @@ struct pd_timer
     uint16_t interval;
     //whether repeat
     uint8_t repeated;
+    uint8_t timer_no;
     //the function pointer to callback if expiry
-    void (*expiry_cb)(void);
+    expiry_cb timer_cb;
+    void* arg;
 };
 
-/******************************************************************************
- * FunctionName : timer1_init
- * Description  : init timer1.
- * Parameters   : timer_cfg: the config timer.
- * Returns      : 
- ******************************************************************************/
-void timer1_init(struct pd_timer timer_cfg);
 
-/******************************************************************************
- * FunctionName : timer1_start
- * Description  : start timer1.
- * Parameters   : 
- * Returns      : 
- ******************************************************************************/
-void timer1_start(void);
+void pando_timer_init(struct pd_timer * timer);
 
-/******************************************************************************
- * FunctionName : timer1_stop
- * Description  : stop timer1.
- * Parameters   : 
- * Returns      : 
- ******************************************************************************/
-void timer1_stop(void);
+void pando_timer_start(struct pd_timer * timer);
+
+void pando_timer_stop(struct pd_timer * timer);
+
 
 #endif /* _PANDO_TIMER_H_ */
